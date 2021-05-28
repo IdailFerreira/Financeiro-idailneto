59dc6ea0e286c13c254b15a9673fe0b0103854f8
Idail Ferreira <neto_br_8@hotmail.com>
2021-05-28 18:43:52 -0400

    Alterações em outra branch.

--- Financeiro Software/Program.cs
+++ Financeiro Software/Program.cs
@@ -8,9 +8,24 @@ namespace Financeiro_Software
 {
     class Program
     {
+        int i = 0;
         static void Main(string[] args)
         {
             Console.WriteLine("olá 1");
+            Console.WriteLine("olá 2");
+            Console.WriteLine("olá 3");
+
+            Console.WriteLine("Idail Ferreira de Vasconcelos Neto");
+            Console.WriteLine("28 anos");
+
+            Console.ReadKey();
+
+            for(int i = 0;i < 10;i++)
+            {
+                Console.WriteLine("oi");
+            }
+
+            //verificando alterações
         }
     }
 }
