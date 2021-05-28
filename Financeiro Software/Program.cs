a81a109e6a7c64026059bad5312a6de81a01b8ff
Idail Ferreira <neto_br_8@hotmail.com>
2021-05-28 03:40:07 -0400

    Teste outro repositorio

--- Financeiro Software/Program.cs
+++ Financeiro Software/Program.cs
@@ -12,6 +12,7 @@ namespace Financeiro_Software
         {
             Console.WriteLine("olá 1");
             Console.WriteLine("olá 2");
+            Console.WriteLine("olá 3");
         }
     }
 }
