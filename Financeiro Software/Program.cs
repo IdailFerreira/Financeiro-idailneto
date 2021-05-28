92b50c334c89c0caa1c2b4f651739a017bffb7a5
Idail Ferreira <neto_br_8@hotmail.com>
2021-05-28 03:30:47 -0400

    Primeiro envio de alterações do mestre.

---
+++ Financeiro Software/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financeiro_Software
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("olá 1");
+        }
+    }
+}
